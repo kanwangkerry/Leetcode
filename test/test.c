17ef7f911c247318e3342094685964949f5115d7
kanwangnyu <kan.wang@nyu.edu>
2013-11-06 23:20:15 -0500

    update

--- test/test.c
+++ test/test.c
@@ -1,70 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
-#include <tr1/unordered_set>
 #include <set>
 #include <vector>
 #include <map>
 #include <queue>
 using namespace std;
 
-    bool ableToDecode2(string s, int begin){
-        if(begin == s.size() -1 || begin < 0)
-            return false;
-        char a[2];
-        a[0] = s[begin+0];
-        a[1] = s[begin+1];
-        int x = atoi(a);
-        if(x>=10 && x <= 26)
-            return true;
-        else return false;
-    }
 
-    int numDecodings(string s) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        int n = s.size();
-        if(n ==0)
-            return 0;
-        int *result = new int[n];
-            
-        if(s[n-1] == '0'){
-            result[n-1] = 0;
-        }
-        else{
-            result[n-1] = 1;
-        }
-        if(s.size() >= 2 && ableToDecode2(s, s.size()-2)){
-            result[s.size()-2] = 1 + result[s.size()-1];
-        }
-        else if(s.size() >= 2 && !ableToDecode2(s, s.size()-2)){
-            if(s[s.size()-2] == '0')
-                result[s.size()-2] = 0;
-            else
-                result[s.size()-2] = result[s.size()-1];
-        }
-        for(int i = s.size() - 3; i >= 0 ; i--){
-            if(s[i] == '0'){
-                result[i] = 0;
-                continue;
-            }
-            if(ableToDecode2(s, i)){
-                result[i] = result[i+1] + result[i+2];
-            }
-            else
-                result[i] = result[i+1];
-        }
-        return result[0];
-        
-    }
-    
-    
-int main(){
-	string s("abc");
-	s.push_back('\0');
-	s += "def";
-	int index = s.find('\0');
+struct TrieNode{
+public:
+	vector<TrieNode*> child;
+	TrieNode(){
+		child = vector<TrieNode*>(26, NULL);
+	}
+};
 
-	printf("%s\n", s.substr(index+1, s.size()-index - 1).c_str());
-	return 1;
+void insert(string s, TrieNode* root){
+	int index = 0;
+	int n = s.size();
+	while(index < n){
+		if(root->child[s[index] - 'a']){
+			root = root->child[s[index++] - 'a'];
+		}
+		else{
+			TrieNode *t = new TrieNode;
+			root->child[s[index++]- 'a'] = t;
+			root = t;
+		}
+	}
 }
+
+bool find(string s, TrieNode* root){
+	int index = 0;
+	int n = s.size();
+	while(index < n){
+		if(root->child[s[index] - 'a']){
+			root = root->child[s[index++] - 'a'];
+		}
+		else{
+			return false;
+		}
+	}
+	return true;
+}
+int main(){
+	TrieNode *root = new TrieNode;
+	insert("abc", root);
+	insert("def", root);
+	insert("abd", root);
+	insert("ded", root);
+	if(find("abc", root))
+		printf("abc found\n");
+	else
+		printf("abc not found\n");
+	if(find("deg", root))
+		printf("deg found\n");
+	else
+		printf("deg not found\n");
+	return 0;
+	
+
+}
+
